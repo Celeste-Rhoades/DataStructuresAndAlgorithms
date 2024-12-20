720c7a79545ed0fd3d8f8ffe8540437d105b0409
Celeste-Rhoades <celesteisthebest82@gmail.com>
2024-12-20 10:59:44 -0700

    Added a differnet way to solve it

--- maxValue.js
+++ maxValue.js
@@ -5,11 +5,12 @@
 // You can assume that the array is non-empty.
 
 const maxValue = nums => {
-  let max = -Infinity;
-  for (let num of nums) {
-    if (num > max) {
-      max = num;
-    }
-  }
-  return max;
+  // let max = -Infinity;
+  // for (let num of nums) {
+  //   if (num > max) {
+  //     max = num;
+  //   }
+  // }
+  // return max;
+  return Math.max(...nums);
 };
