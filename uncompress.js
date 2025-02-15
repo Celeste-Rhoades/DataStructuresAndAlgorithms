b55d1ece04dd01505f60e43c4f3013e016febb9f
Celeste-Rhoades <celesteisthebest82@gmail.com>
2025-02-15 16:00:31 -0700

    uncompressedChars

---
+++ uncompress.js
@@ -0,0 +1,33 @@
+// Write a function, uncompress, that takes in a string as an argument. The input string will be formatted into multiple groups according to the following pattern:
+
+// <number><char>
+
+// for example, '2c' or '3a'.
+// The function should return an uncompressed version of the string where each 'char' of a group is repeated 'number' times consecutively. You may assume that the input string is well-formed according to the previously mentioned pattern.
+
+const uncompress = s => {
+  // todo
+  let numbers = "0123456789";
+  let result = "";
+  let i = 0;
+  let j = 0;
+  while (j < s.length) {
+    if (numbers.includes(s[j])) {
+      j += 1;
+    } else {
+      const num = Number(s.slice(i, j));
+      for (let count = 0; count < num; count += 1) {
+        result += s[j];
+      }
+      j += 1;
+      i = j;
+    }
+  }
+
+  return result;
+};
+uncompress("2c3a1t"); // -> 'ccaaat'
+uncompress("4s2b"); // -> 'ssssbb'
+uncompress("2p1o5p"); // -> 'ppoppppp'
+uncompress("3n12e2z"); // -> 'nnneeeeeeeeeeeezz'
+uncompress("127y"); // ->'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy'
