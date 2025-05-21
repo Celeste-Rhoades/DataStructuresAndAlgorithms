ecb5930577a20de5a1fbc2df0d5d5bcee2830edd
Celeste-Rhoades <celesteisthebest82@gmail.com>
2025-05-21 15:18:25 -0600

    factorial

---
+++ recursion/sumOfLengths.js
@@ -0,0 +1,9 @@
+const sumOfLengths = strings => {
+  if (strings.length === 0) {
+    return 0;
+  }
+  return strings[0].length + sumOfLengths(strings.slice(1));
+};
+
+console.log(sumOfLengths(["goat", "cat", "purple"]));
+console.log(sumOfLengths(["bike", "at", "pencils", "phone"])); // -> 18
