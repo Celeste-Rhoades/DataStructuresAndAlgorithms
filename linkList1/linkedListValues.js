e8a0cd7eab8c711023ed8bf471aa321e7bcc7e3e
Celeste-Rhoades <celesteisthebest82@gmail.com>
2025-05-28 12:02:55 -0600

    link list values

---
+++ linkList1/linkedListValues.js
@@ -0,0 +1,11 @@
+const linkedListValues = head => {
+  const values = [];
+  _linkedListValues(head, values);
+  return values;
+};
+
+const _linkedListValues = (head, values) => {
+  if (head === null) return;
+  values.push(head.val);
+  _linkedListValues(head.next, values);
+};
